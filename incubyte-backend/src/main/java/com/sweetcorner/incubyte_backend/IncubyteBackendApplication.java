e7ee9302fc3badf6ddacd4959235584e4ee22f56
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-12-13 16:02:28 +0530

    first commit

---
+++ incubyte-backend/src/main/java/com/sweetcorner/incubyte_backend/IncubyteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sweetcorner.incubyte_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubyteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncubyteBackendApplication.class, args);
+	}
+
+}
---
+++ incubyte-backend/src/test/java/com/sweetcorner/incubyte_backend/IncubyteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweetcorner.incubyte_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncubyteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
